4afd3b7952deb1cb182e66f0924f679a3d9f6d4f
kenchang198 <sasamotoken@gmail.com>
2023-08-31 19:52:06 +0900

    Project フィーチャーテスト setUp

--- tests/Feature/Project/ProjectTest.php
+++ tests/Feature/Project/ProjectTest.php
@@ -9,48 +9,56 @@ use Faker\Factory as FakerFactory;
 
 class ProjectTest extends FeatureBaseTestCase
 {
+    protected $user;
+    protected $prj_member_ids;
+    protected $project_name;
+    
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        // プロジェクトへメンバー追加
+        $this->prj_member_ids = [
+            $this->user->id,
+            User::factory()->create()->id,
+            User::factory()->create()->id,
+            User::factory()->create()->id
+        ];
+        
+        $this->project_name = FakerFactory::create()->text(20);
+    }
+
     // プロジェクト作成フォーム表示テスト
     public function test_can_create_project()
     {
-        $user = User::factory()->create();
-        $response =  $this->actingAs($user)->get('/project/create');
+        $response =  $this->actingAs($this->user)->get('/project/create');
         $response->assertStatus(200);
     }
 
     // プロジェクトの保存テスト
     public function test_can_store_project()
     {
-        $user = User::factory()->create();
-        
-        // プロジェクトへメンバー追加
-        $prj_member_ids = [
-            $user->id,
-            User::factory()->create()->id,
-            User::factory()->create()->id,
-            User::factory()->create()->id
-        ];
-        
-        $project_name = FakerFactory::create()->text(20);
-        
-        $response = $this->actingAs($user)->post('/project/store', [
-            'project_name' => $project_name,
-            'responsible_person_id' => $user->id,
-            'user_id' => $prj_member_ids
+        $response = $this->actingAs($this->user)->post('/project/store', [
+            'project_name' => $this->project_name,
+            'responsible_person_id' => $this->user->id,
+            'user_id' => $this->prj_member_ids
         ]);
     
         $response->assertStatus(302);
     
         $this->assertDatabaseHas('projects', [
-            'project_name' => $project_name,
-            'responsible_person_id' => $user->id,
-            'created_user_id' => $user->id,
-            'updated_user_id' => $user->id
+            'project_name' => $this->project_name,
+            'responsible_person_id' => $this->user->id,
+            'created_user_id' => $this->user->id,
+            'updated_user_id' => $this->user->id
         ]);
         
         $project = Project::latest('id')->first();
 
         // 関連テーブルへのプロジェクトメンバーの保存結果を確認
-        foreach ($prj_member_ids as $id) {
+        foreach ($this->prj_member_ids as $id) {
             $this->assertDatabaseHas('project_user', [
                 'project_id' => $project->id,
                 'user_id'=> $id
