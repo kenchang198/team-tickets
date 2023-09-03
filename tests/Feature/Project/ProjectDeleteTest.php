cf38469ca8a3d19f4ec86d33fca5ed14506725a8
kenchang198 <sasamotoken@gmail.com>
2023-09-03 09:42:02 +0900

    テスト プロジェクト削除 関連テーブルの確認

--- tests/Feature/Project/ProjectDeleteTest.php
+++ tests/Feature/Project/ProjectDeleteTest.php
@@ -25,10 +25,22 @@ class ProjectDeleteTest extends FeatureBaseTestCase
     {
         $url = route('project.delete', $this->project);
 
+        $members = $this->project->users;
+
         $response = $this->actingAs($this->user)->delete($url);
 
         $response->assertStatus(302);
 
         $this->assertDatabaseMissing('projects', ['id' => $this->project->id]);
+
+        foreach ($members as $member) {
+            $project_member = [
+                'project_id' => $this->project->id,
+                'user_id' => $member->id
+            ];
+            // プロジェクト - メンバーの関連情報も削除されているか確認する
+            $this->assertDatabaseMissing('project_user', $project_member);
+        }
+        
     }
 }
